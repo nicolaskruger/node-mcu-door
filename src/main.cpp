25cf7bcf57361ec69879ad895c4de07fde8797ee
nkruger <nicolas.nkruger@gmail.com>
2022-06-05 17:13:24 -0300

    setUp []

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include "Arduino.h"
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(D1, INPUT_PULLUP);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  
+  bool turnOn = digitalRead(D1);
+
+  if(turnOn){
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else 
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  
+  // wait for a second
+  delay(1000);
+}
